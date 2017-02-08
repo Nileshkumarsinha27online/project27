1e11c0e0ab891979d06a58db7b32457cc22c2c38
Nileshkumarsinha27online <nsinha18@sapient.com>
2017-02-08 16:37:18 +0530

    Add files via upload

---
+++ src/Tabdata/index.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './styles.scss';
+export default class TabData extends Component{
+	render(){
+		return(
+
+			<div className='tabData'>
+				<p className='tabDesc'>
+					
+				</p>
+			</div>
+			)
+	}
+}
